896e2d2ed49ea527e61105aba2298c52551c0741
Balaji Patil <balajipatil3252@gmail.com>
2026-04-16 09:49:30 +0530

    create auth model

--- backend/src/models/authModel.js
+++ backend/src/models/authModel.js
@@ -1,44 +1,36 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/sqliteDB');
-const bcrypt = require('bcrypt');
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/sqliteDB");
 
-const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
+const User = sequelize.define(
+  "User",
+  {
+    user_id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
     username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true,
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-}, {
-    tableName: 'Users',
-    hooks: {
-        beforeSave: async (user) => {
-            if (user.changed('password')) {
-                const salt = await bcrypt.genSalt(10);
-                user.password = await bcrypt.hash(user.password, salt);
-            }
-        },
-    },
-});
-
-User.prototype.validPassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-};
+  },
+  {
+    tableName: "Users",
+    timestamps: "true",
+  },
+);
 
 module.exports = User;
